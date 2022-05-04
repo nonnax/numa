6eed0637ca00f8e994414ac33b1019b54e85f99e
nonnax <ironald@gmail.com>
2022-05-04 10:45:51 +0800

    refactor

--- lib/numa.rb
+++ lib/numa.rb
@@ -25,7 +25,7 @@ class Numa
   end
 
   def on(u, **params)
-    return unless match(u, **params)
+    return unless @matched=match(u, **params)
     yield *@captures
   end
 
@@ -43,7 +43,7 @@ class Numa
   def put;    yield if req.put? end
   def delete; yield if req.delete? end
 
-  def not_found; res.status = 404; run_once{ yield } end
+  def not_found; return if @matched; res.status = 404; run_once{ yield } end
 
   def match(u, **params)
     req.path_info.match(pattern(u))
